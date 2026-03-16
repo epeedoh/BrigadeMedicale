4ac2dd33ff177f0b8da34570b8745300534ff8e9
epeedoh <epeedoh@github.com>
2026-03-16 19:40:50 +0000

    Fix using directives: use EntityFrameworkCore not Infrastructure

--- src/BrigadeMedicale.API/Controllers/SetupController.cs
+++ src/BrigadeMedicale.API/Controllers/SetupController.cs
@@ -1,7 +1,6 @@
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore.Infrastructure;
-using System.Data;
+using Microsoft.EntityFrameworkCore;
 using BrigadeMedicale.Infrastructure.Data;
 
 namespace BrigadeMedicale.API.Controllers;
